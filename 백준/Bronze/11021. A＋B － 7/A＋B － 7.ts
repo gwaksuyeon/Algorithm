a115ac9e08bfe93fdd6e231e9ca6883a21b9c8ea
Gwak Suyeon <51697335+gwaksuyeon@users.noreply.github.com>
2022-12-11 21:38:47 +0900

    [Bronze V] Title: A+B - 7, Time: 164 ms, Memory: 9508 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.ts
@@ -0,0 +1,10 @@
+import fs = require('fs')
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+const count = Number(input[0]);
+
+for(let i=0; i<count; i++) {
+    const array = input[i+1].split(' ');
+    
+   console.log(`Case #${i+1}: ${Number(array[0]) + Number(array[1])}`)
+}
