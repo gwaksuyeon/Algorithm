aae21691c5433c6abcb232c1daae1d2bce2d83b2
Gwak Suyeon <51697335+gwaksuyeon@users.noreply.github.com>
2022-12-11 15:51:37 +0900

    [Bronze V] Title: 시험 성적, Time: 144 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.ts
@@ -0,0 +1,16 @@
+import fs = require('fs')
+const input = fs.readFileSync('/dev/stdin').toString().trim();
+
+const score = Number(input);
+
+if(score >= 90) {
+    console.log('A')
+} else if (score < 90 && score >= 80) {
+    console.log('B')
+} else if (score < 80 && score >= 70) {
+    console.log('C')
+} else if (score < 70 && score >= 60) {
+    console.log('D')
+} else {
+    console.log('F')
+}
