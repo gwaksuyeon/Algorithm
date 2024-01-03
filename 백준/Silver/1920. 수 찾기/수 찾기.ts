2d5ddcdd810896f9b9f49e35971eb22c63561e12
Gwak Suyeon <51697335+gwaksuyeon@users.noreply.github.com>
2024-01-03 23:10:04 +0900

    [Silver IV] Title: 수 찾기, Time: 296 ms, Memory: 41188 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.ts
+++ 백준/Silver/1920. 수 찾기/수 찾기.ts
@@ -1,16 +1,16 @@
 const input = require('fs').readFileSync('dev/stdin').toString().trim().split('\n');
-const n_arr = input[1].split(' ').map(v=>+v);
-const m_arr = input[3].split(' ').map(v=>+v);
 
-const n_set = new Set(n_arr); 
+const NArray = new Set(input[1].split(" ").map(Number));
+const MArray = input[3].split(" ").map(Number);
 
-const result = [];
+let result: number[] = [];
 
-m_arr.forEach(m => { 
-    if(n_set.has(m)){
-        result.push(1);
-    }
-    else result.push(0);
+MArray.forEach((obj) => {
+  if (NArray.has(obj)) {
+    result.push(1);
+  } else {
+    result.push(0);
+  }
 });
 
-console.log(result.join('\n'));
+console.log(result.join("\n"));
